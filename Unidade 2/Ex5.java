777a45640aa8f94faae9a0074eae1f9c144fa564
João Rambo <jpramboguanabara@hotmail.com>
2022-03-15 20:45:15 -0300

    ex 5

---
+++ Unidade 2/Ex5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Leia quatro valores inteiros A, B, C e D. 
+        // A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D 
+        // segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+        System.out.println("Digite o valor A: ");
+        int valueA = input.nextInt();
+
+        System.out.println("Digite o valor B: ");
+        int valueB = input.nextInt();
+
+        System.out.println("Digite o valor C: ");
+        int valueC = input.nextInt();
+
+        System.out.println("Digite o valor D: ");
+        int valueD = input.nextInt();
+
+        int result = (valueA * valueB) - (valueC * valueD);
+
+        System.out.println("O resultado é " + result);
+    } 
+}
