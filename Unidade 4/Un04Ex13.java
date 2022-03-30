fbd84a8d963f958918c3fa037a52cc9e23d7570f
João Rambo <jpramboguanabara@hotmail.com>
2022-03-30 00:00:17 -0300

    increment no truco

--- Unidade 4/Un04Ex13.java
+++ Unidade 4/Un04Ex13.java
@@ -15,8 +15,8 @@ public class Un04Ex13 {
         */
 
         Scanner input = new Scanner(System.in);
-        int AS = 1, dois = 2, tres = 3, sete = 7, valete = 11, dama = 12, rei = 13;
-        int card1, card2, card3;
+        int AS = 1, dois = 2, tres = 3;
+        int card1, card2, card3, contador = 0;
 
         System.out.println("Insira uma carta em mãos: ");
         card1 = input.nextInt();
@@ -27,25 +27,25 @@ public class Un04Ex13 {
         System.out.println("Insira uma carta em mãos: ");
         card3 = input.nextInt();
 
-        if ((
-            ((card1 == AS || card1 == dois || card1 == tres) || (card2 == AS || card1 == dois || card2 == tres) || (card3 == AS || card3 == dois || card3 == tres))
-            && 
-            ((card2 != AS && card2 != dois && card3 != tres) && (card3 != AS && card3 != dois && card3 != tres)) || ((card1 != AS && card1 != dois && card1 != tres) && (card3 != AS && card3 != dois && card3 != tres)) || ((card1 != AS && card1 != dois && card1 != tres) && (card2 != AS && card2 != dois && card2 != tres))
-            )) {
-            System.out.println("TRUCO");
-        }  else if (
-            ((card1 == AS || card1 == dois || card1 == tres) && (card2 == AS || card2 == dois || card2 == tres) && (card3 == AS || card3 == dois || card3 == tres))
-        ) {
-            System.out.println("NOVE");
-        } else if ((
-            ((card1 == AS || card1 == dois || card1 == tres) && (card2 == AS || card2 == dois || card2 == tres)) || ((card1 == AS || card1 == dois || card1 == tres) && (card3 == AS || card3 == dois || card3 == tres)) || ((card2 == AS || card2 == dois || card2 == tres) && (card3 == AS || card3 == dois || card3 == tres))
-            &&
-            ((card1 != AS && card1 != dois && card1 != tres) || (card2 != AS && card2 != dois && card2 != tres) || (card3 != AS && card3 != dois && card3 != tres))
-            )
-        ) {
-            System.out.println("SEIS");
-        }  
+        if (card1 == AS || card1 == dois || card1 == tres) {
+            contador++;
+        } 
+        if (card2 == AS || card2 == dois || card2 == tres) {
+            contador++;
+        }
+        if (card3 == AS || card3 == dois || card3 == tres) {
+            contador++;
+        }
 
+        if (contador == 1) {
+            System.out.println("TRUCO");
+        }
+        if (contador == 2) {
+            System.out.println("SEIS");
+        }
+        if (contador == 3) {
+            System.out.println("NOVE");
+        }
         input.close();
     }
 }
