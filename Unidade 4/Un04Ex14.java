e2756ff5c6a3aa49479365f46bebf3e79c6ea82b
João Rambo <jpramboguanabara@hotmail.com>
2022-04-04 21:33:07 -0300

    14 - 18

---
+++ Unidade 4/Un04Ex14.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Un04Ex14 {
+    public static void main(String[] args) {
+        /*
+        Leia uma data e determine se ela é válida. Ou seja, verifique se o mês está entre 1 e 12, 
+        e se o dia existe naquele mês. Note que fevereiro tem 29 dias em anos bissextos, 
+        e 28 dias em anos não bissextos.
+        Para resolver este problema pode se utilizar do algoritmo descrito no fluxograma:
+        */
+
+        Scanner input = new Scanner(System.in);
+        boolean bissexto;
+        int day, month, year;
+
+        System.out.println("Insira o dia: ");
+        day = input.nextInt();
+
+        System.out.println("Insira o dia: ");
+        month = input.nextInt();
+
+        System.out.println("Insira o dia: ");
+        year = input.nextInt();
+
+        if (month < 0 && month > 12) {
+            System.out.println("Mês inexistente");
+        }
+
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            System.out.println("O mês tem 31 dias");
+            if (day < 0) {
+                System.out.println("Dia inexistente nesse mês");
+            } else if (day > 31) {
+                System.out.println("Dia inexistente nesse mês");
+            }
+        }
+
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            System.out.println("O mês tem 30 dias");
+            if (day < 0) {
+                System.out.println("Dia inexistente nesse mês");
+            } else if (day > 30) {
+                System.out.println("Dia inexistente nesse mês");
+            }
+        } else if (month == 2) {
+            System.out.println("O ano é bissexto? Digite true ou false");
+            bissexto = input.nextBoolean();
+
+            if (bissexto) {
+                if (day > 29) {
+                    System.out.println("Dia inexistente nesse mês"); 
+                } else if (day < 0) {
+                    System.out.println("Dia inexistente nesse mês");
+                } else {
+                    System.out.println("Data válida");
+                }
+            } else {
+                if (day > 28) {
+                    System.out.println("Dia inexistente nesse mês"); 
+                } else {
+                    System.out.println("Data válida");
+                }
+            }
+        }
+
+        
+
+        /*
+1 	Janeiro 	tem 31 dias
+2 	Fevereiro 	tem 28 dias (29 dias nos anos bissextos)
+3 	Março 	tem 31 dias
+4 	Abril 	tem 30 dias
+5 	Maio 	tem 31 dias
+6 	Junho 	tem 30 dias
+7 	Julho 	tem 31 dias
+8 	Agosto 	tem 31 dias
+9 	Setembro 	tem 30 dias
+10 	Outubro 	tem 31 dias
+11 	Novembro 	tem 30 dias
+12 	Dezembro 	tem 31 dias
+        */
+        
+    }
+}
---
+++ Unidade 4/Un04Ex16.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Un04Ex16 {
+    public static void main(String[] args) {
+        /* 
+        Escreva um algoritmo que leia a idade de 2 homens e 2 mulheres 
+        (considere que a idade entre homens e mulheres sempre serão diferentes). 
+        Calcule e escreva a soma das idades do homem mais velho com a mulher mais nova, 
+        e o produto das idades do homem mais novo com a mulher mais velha.
+        */
+
+        Scanner input = new Scanner(System.in);
+
+        int idadeH1, idadeH2, idadeM1, idadeM2;
+        int result1, result2;
+
+        System.out.println("Insira a idade do homem 1");
+        idadeH1 = input.nextInt();
+
+        System.out.println("Insira a idade do homem 2");
+        idadeH2 = input.nextInt();
+
+        System.out.println("Insira a idade da mulher 1");
+        idadeM1 = input.nextInt();
+
+        System.out.println("Insira a idade da mulher 2");
+        idadeM2 = input.nextInt();
+
+        if (idadeH1 > idadeH2) {
+            if (idadeM1 < idadeM2) {
+                result1 = idadeH1 + idadeM1; 
+                result2 = idadeH2 * idadeM2;
+            } else {
+                result1 = idadeH1 + idadeM2;
+                result2 = idadeH2 * idadeM1;
+            } 
+        } else {
+            if (idadeM1 < idadeM2) {
+                result1 = idadeH2 + idadeM1;
+                result2 = idadeH1 * idadeM2;
+            } else {
+                result1 = idadeH2 + idadeM2;
+                result2 = idadeH1 * idadeM1;
+            }
+        }
+
+        System.out.println("result1: " + result1); 
+        System.out.println("result2: " + result2); 
+    }
+}
