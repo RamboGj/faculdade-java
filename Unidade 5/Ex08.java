125793517027df444b71b854e3ae01d57596d086
João Rambo <jpramboguanabara@hotmail.com>
2022-05-16 21:16:27 -0300

    aasdadsadsa

---
+++ Unidade 5/Ex08.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ex08 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        /*
+        Descreva um algoritmo que leia um número inteiro n e, dados n números inteiros, 
+        descreva um algoritmo que:
+
+        escreva o menor valor negativo;
+        escreva a média dos números positivos.
+
+        */
+
+        int n = input.nextInt();
+        int lowest = 0, highest = 0, positive = 0;
+
+        for (int i = 0; i < n; i ++) {
+            int value = input.nextInt();
+
+            if (value < 0 && value < lowest) {
+                lowest = value;
+            }
+
+            if (value > 0) {
+                positive ++;
+                highest += value;
+            }
+        }
+
+        System.out.println("O menor valor negativo é " + lowest + " e a média dos maiores valores é " + highest / positive);
+
+        input.close();
+    }
+}
+
