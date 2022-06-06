9b1fd896a21dd2aef916d1f426d33d4acbb36b1e
João Rambo <jpramboguanabara@hotmail.com>
2022-06-06 17:39:52 -0300

    ex 03

--- Unidade 6/Un06Ex01.java
+++ Unidade 6/Un06Ex01.java
@@ -8,17 +8,16 @@ public class Un06Ex01 {
          Escreva na ordem inversa em que foram lidos. Faça um método para ler e outro para escrever.
         */
         Scanner input = new Scanner(System.in);
-        
-        int tamanhoVetor = 5;
-        int numeros[] = new int[tamanhoVetor], number, inverted[] = new int[tamanhoVetor];
 
-        for (int i = 0; i < tamanhoVetor; i ++) {
+        int numeros[] = new int[10], number, inverted[] = new int[10];
+
+        for (int i = 0; i < numeros.length; i ++) {
             number = input.nextInt();
             numeros[i] = number;
         }
 
-        for (int i = 0; i < tamanhoVetor; i++) {
-            inverted[i] = numeros[tamanhoVetor - 1 - i];
+        for (int i = 0; i < numeros.length; i++) {
+            inverted[i] = numeros[numeros.length - 1 - i];
         }
 
         System.out.println(Arrays.toString(inverted));
---
+++ Unidade 6/Un06Ex03.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Un06Ex03 {
+    public static void main(String[] args) {
+        /*
+         Descreva um algoritmo que leia 12 valores reais e os coloque em um vetor de 12 posições 
+         do tipo real. Em seguida, modifique o vetor de modo que os valores das posições ímpares 
+         sejam aumentados em 5% e os das posições pares sejam aumentados em 2%. 
+         Imprima o vetor resultante. Faça um método para ler os valores, outro para ajustar os
+         valores dentro do vetor e outro para escrever os valores atualizados do vetor.
+        */
+        Scanner input = new Scanner(System.in);
+        double numbers[] = new double[12], number = 0.0;
+
+        double result[] = readValues(numbers, number);
+        double finalResult[] = refactorValues(result);
+
+        printRefactoredArray(finalResult);
+        input.close();
+    }
+
+    private static double[] refactorValues(double array[]) {
+        for (int i = 0; i < array.length; i ++) {
+            if (array[i] % 2 != 0) {
+                array[i] = array[i] * 0.05;
+            } 
+            if (i % 2 == 0) {
+                array[i] = array[i] * 0.02;
+            }
+        }
+        return array;
+    }
+
+    private static void printRefactoredArray(double array[]) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static double[] readValues(double array[], double number) {
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i ++) {
+            number = input.nextInt();
+            array[i] = number; 
+        }
+
+        return array;
+    } 
+}
